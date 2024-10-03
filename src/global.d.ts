d679dde033dfb495c3fced4529d6c596006d7763
zobweyt <zobweyt@gmail.com>
2024-10-03 20:07:23 +0300

    Wrap documentation

--- src/global.d.ts
+++ src/global.d.ts
@@ -22,7 +22,8 @@ interface ImportMetaEnv {
   readonly VITE_SESSION_SECRET: string;
 
   /**
-   * Determines whether the cookies sent to browser should be marked as [secure](https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies#block_access_to_your_cookies).
+   * Determines whether the cookies sent to browser should be
+   * marked as [secure](https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies#block_access_to_your_cookies).
    */
   readonly VITE_SECURE_COOKIES: string;
 }
