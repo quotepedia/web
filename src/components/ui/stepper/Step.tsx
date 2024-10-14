a4ef7413997044393605d18e2f1ae208c427705b
zobweyt <zobweyt@gmail.com>
2024-10-14 22:14:35 +0300

    fix: properly align step

--- src/components/ui/stepper/Step.tsx
+++ src/components/ui/stepper/Step.tsx
@@ -1,18 +1,21 @@
 import type { JSX, ValidComponent } from "solid-js";
-import { createEffect, ParentProps } from "solid-js";
+import { createEffect, ParentProps, splitProps } from "solid-js";
 import createPersistent from "solid-persistent";
 
 import { Polymorphic, PolymorphicProps } from "@kobalte/core/polymorphic";
 
+import { cn } from "~/lib/utils/css";
 import useStepperContext from "./context";
 
-export type StepperStepProps = ParentProps & {
-  index?: number;
-  onEnter?: () => any;
-};
+export type StepperStepProps = ParentProps &
+  JSX.StylableSVGAttributes & {
+    index?: number;
+    onEnter?: () => any;
+  };
 
 export const StepperStep = <T extends ValidComponent = "div">(props: PolymorphicProps<T, StepperStepProps>) => {
   const context = useStepperContext();
+  const [scopedProps, otherProps] = splitProps(props, ["class"]);
 
   const getPersistentStep = () => context.steps.get(context.currentIndex);
 
@@ -23,7 +26,14 @@ export const StepperStep = <T extends ValidComponent = "div">(props: Polymorphic
         props.index === context.currentIndex && props.onEnter?.();
       });
 
-      return <Polymorphic as={"li"} aria-current="step" {...props} />;
+      return (
+        <Polymorphic
+          as={"li"}
+          class={cn("flex items-center justify-center", scopedProps.class)}
+          aria-current="step"
+          {...otherProps}
+        />
+      );
     });
 
     context.steps.set(context.currentIndex, step);
