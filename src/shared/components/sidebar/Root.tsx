302f9c15c9a1d69095c7ab3dcbde842fa644fc50
zobweyt <zobweyt@gmail.com>
2024-11-04 13:50:28 +0300

    fix: improve sidebar padding

--- src/shared/components/sidebar/Root.tsx
+++ src/shared/components/sidebar/Root.tsx
@@ -3,7 +3,7 @@ import { type ParentComponent } from "solid-js";
 export const SidebarRoot: ParentComponent = (props) => {
   return (
     <nav
-      class="flex w-full gap-1 border-bg-tertiary md:bg-bg-default max-md:bg-bg-default/75 py-2 backdrop-blur-xl transition-colors max-md:justify-between max-md:border-t md:flex-col md:px-4 md:pt-6 max-md:pb-safe-or-3"
+      class="flex w-full gap-1 border-bg-tertiary md:bg-bg-default max-md:bg-bg-default/75 py-2 backdrop-blur-xl transition-colors max-md:justify-between max-md:border-t md:flex-col md:px-4 md:py-6 max-md:pb-safe-or-3"
       {...props}
     />
   );
