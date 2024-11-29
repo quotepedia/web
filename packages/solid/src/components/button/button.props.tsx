7a1a36dbc118445bd862b72b924b096a08012928
zobweyt <zobweyt@gmail.com>
2024-11-29 21:22:17 +0300

    refactor: improve props

--- packages/solid/src/components/button/button.props.tsx
+++ packages/solid/src/components/button/button.props.tsx
@@ -1,12 +1,14 @@
-import type { JSX, ParentProps, ValidComponent } from "solid-js";
-
-import * as ButtonPrimitive from "@kobalte/core/button";
+import type { ButtonRootProps } from "@kobalte/core/button";
+import type { ParentProps, ValidComponent } from "solid-js";
 import type { VariantProps } from "tailwind-variants";
+import type { styles } from "./button.styles";
 
-import { styles } from "./button.styles";
+export type ButtonRenderProps = {
+  class?: string;
+};
 
 export type ButtonVariantProps = VariantProps<typeof styles>;
 
-export type ButtonProps<T extends ValidComponent = "button"> = ParentProps<ButtonPrimitive.ButtonRootProps<T>> &
-  ButtonVariantProps &
-  JSX.StylableSVGAttributes;
+export type ButtonProps<T extends ValidComponent | HTMLElement = HTMLElement> = ParentProps<
+  ButtonRootProps<T> & ButtonVariantProps & ButtonRenderProps
+>;
