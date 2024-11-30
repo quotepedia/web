c7d0e3020acd1d3bd05baf88d05cc0d8547cd8a0
zobweyt <zobweyt@gmail.com>
2024-11-30 16:59:39 +0300

    chore: add vite typescript config paths plugin

--- packages/solid/playground/vite.config.ts
+++ packages/solid/playground/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import paths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), paths()],
 });
