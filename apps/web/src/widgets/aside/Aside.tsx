10a71e67781bbb3546b6af94e50bd4074edfc8f9
zobweyt <zobweyt@gmail.com>
2024-12-25 20:08:58 +0300

    feat: use cog icon for settings sidebar item

--- apps/web/src/widgets/aside/Aside.tsx
+++ apps/web/src/widgets/aside/Aside.tsx
@@ -32,7 +32,7 @@ export const Aside = () => {
 
       <Sidebar.Group class="max-lg:basis-1/3 lg:mt-auto">
         <Sidebar.Item href="/settings" class="group">
-          <Show when={currentUser()} fallback={<Sidebar.ItemIcon icon="ion:cog" />}>
+          <Show when={currentUser()} fallback={<Sidebar.ItemIcon icon="f7:gear" />}>
             {(user) => (
               <Sidebar.ItemIcon as={Avatar}>
                 <Avatar.Img src={user().avatar_url && formatStorageObject(user().avatar_url!)} alt={user().email} />
