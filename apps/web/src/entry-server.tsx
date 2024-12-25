bf133f53e4f9aa37ef34f081f0c7daa285a43dc8
zobweyt <zobweyt@gmail.com>
2024-12-25 19:58:55 +0300

    feat: add web manifest

--- apps/web/src/entry-server.tsx
+++ apps/web/src/entry-server.tsx
@@ -13,6 +13,7 @@ export default createHandler((event) => (
           />
           <link rel="icon" href="/favicon.ico" type="image/x-icon" />
           <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+          <link rel="manifest" href="/manifest.json" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" />
