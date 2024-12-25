b783383e985b787627503c3c891114f152da9017
zobweyt <zobweyt@gmail.com>
2024-12-25 20:12:53 +0300

    feat: use new icons for account sections

--- apps/web/src/widgets/settings/components/AccountSecuritySection.tsx
+++ apps/web/src/widgets/settings/components/AccountSecuritySection.tsx
@@ -1,4 +1,4 @@
-import { Icon, Heading, SettingsCard, SettingsGroup, Stack } from "@quotepedia/solid";
+import { Icon, SettingsCard, SettingsGroup, Stack } from "@quotepedia/solid";
 import { A, createAsync, useSubmission } from "@solidjs/router";
 import { Show } from "solid-js";
 import { unauthenticate } from "~/entities/auth";
@@ -16,28 +16,24 @@ export const AccountSecuritySection = () => {
     <Show when={currentUser()}>
       {(user) => (
         <Stack.Vertical class="items-start gap-3">
-          <Heading size="subheading" as="h2">
-            {t("heading")}
-          </Heading>
-
           <SettingsGroup>
             <SettingsCard variant="hover" as={A} href="/settings/account/update-email">
-              <Icon icon="heroicons:envelope-open-16-solid" class="size-4" />
+              <Icon icon="f7:envelope" class="text-fg-accent size-6" />
               <SettingsCard.HeaderGroup>
                 <SettingsCard.Header>{t("cards.email.heading")}</SettingsCard.Header>
                 <SettingsCard.Description>{t("cards.email.description")}</SettingsCard.Description>
               </SettingsCard.HeaderGroup>
               <SettingsCard.Value>{user().email}</SettingsCard.Value>
-              <Icon icon="heroicons:chevron-right-16-solid" class="text-fg-muted size-4" />
+              <Icon icon="f7:chevron-right" class="text-fg-muted size-4" />
             </SettingsCard>
 
             <SettingsCard variant="hover" as={A} href="/settings/account/update-password">
-              <Icon icon="heroicons:key-16-solid" class="size-4" />
+              <Icon icon="f7:asterisk-circle" class="text-fg-accent size-6" />
               <SettingsCard.HeaderGroup>
                 <SettingsCard.Header>{t("cards.password.heading")}</SettingsCard.Header>
                 <SettingsCard.Description>{t("cards.password.description")}</SettingsCard.Description>
               </SettingsCard.HeaderGroup>
-              <Icon icon="heroicons:chevron-right-16-solid" class="text-fg-muted size-4" />
+              <Icon icon="f7:chevron-right" class="text-fg-muted size-4" />
             </SettingsCard>
           </SettingsGroup>
 
@@ -49,12 +45,11 @@ export const AccountSecuritySection = () => {
               aria-busy={unauthenticating.pending}
               variant="hover"
             >
-              <Icon icon="heroicons:arrow-right-on-rectangle-20-solid" class="size-4 text-red-600" />
+              <Icon icon="f7:square-arrow-right" class="size-6 text-red-600" />
               <SettingsCard.HeaderGroup>
                 <SettingsCard.Header class="text-red-600">{t("cards.signout.heading")}</SettingsCard.Header>
-                <SettingsCard.Description>{t("cards.signout.description")}</SettingsCard.Description>
               </SettingsCard.HeaderGroup>
-              <Icon icon="heroicons:chevron-right-16-solid" class="text-fg-muted size-4" />
+              <Icon icon="f7:chevron-right" class="text-fg-muted size-4" />
             </SettingsCard>
           </SettingsGroup>
         </Stack.Vertical>
--- apps/web/src/widgets/settings/components/SettingsAccountSection.tsx
+++ apps/web/src/widgets/settings/components/SettingsAccountSection.tsx
@@ -1,4 +1,4 @@
-import { Icon, Avatar, Heading, SettingsCard, SettingsGroup, Stack } from "@quotepedia/solid";
+import { Avatar, Icon, SettingsCard, SettingsGroup } from "@quotepedia/solid";
 import { A, createAsync } from "@solidjs/router";
 import { Show } from "solid-js";
 import { getCurrentUser } from "~/entities/user";
@@ -10,27 +10,21 @@ export const SettingsAccountSection = () => {
   const currentUser = createAsync(() => getCurrentUser());
 
   return (
-    <Stack.Vertical class="items-start gap-3">
-      <Heading as="h2" size="subheading">
-        {t("heading")}
-      </Heading>
-
-      <SettingsGroup>
-        <SettingsCard variant="hover" as={A} href="/settings/account">
-          <Show when={currentUser()} fallback={<Icon icon="heroicons:user-circle-16-solid" class="size-4" />}>
-            {(user) => (
-              <Avatar class="size-4">
-                <Avatar.Img src={user().avatar_url && formatStorageObject(user().avatar_url!)} alt={user().email} />
-              </Avatar>
-            )}
-          </Show>
-          <SettingsCard.HeaderGroup>
-            <SettingsCard.Header>{t("cards.account.heading")}</SettingsCard.Header>
-            <SettingsCard.Description>{t("cards.account.description")}</SettingsCard.Description>
-          </SettingsCard.HeaderGroup>
-          <Icon icon="heroicons:chevron-right-16-solid" class="text-fg-muted size-4" />
-        </SettingsCard>
-      </SettingsGroup>
-    </Stack.Vertical>
+    <SettingsGroup>
+      <SettingsCard variant="hover" as={A} href="/settings/account">
+        <Show when={currentUser()} fallback={<Icon icon="f7:person-crop-circle-fill" class="text-fg-accent size-6" />}>
+          {(user) => (
+            <Avatar class="size-6">
+              <Avatar.Img src={user().avatar_url && formatStorageObject(user().avatar_url!)} alt={user().email} />
+            </Avatar>
+          )}
+        </Show>
+        <SettingsCard.HeaderGroup>
+          <SettingsCard.Header>{t("cards.account.heading")}</SettingsCard.Header>
+          <SettingsCard.Description>{t("cards.account.description")}</SettingsCard.Description>
+        </SettingsCard.HeaderGroup>
+        <Icon icon="f7:chevron-right" class="text-fg-muted size-4" />
+      </SettingsCard>
+    </SettingsGroup>
   );
 };
