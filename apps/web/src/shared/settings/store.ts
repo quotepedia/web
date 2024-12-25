ec5dee94119a46cfdc33fdac168604df586670da
zobweyt <zobweyt@gmail.com>
2024-12-25 20:10:09 +0300

    feat: add snowfall component

--- apps/web/src/shared/settings/store.ts
+++ apps/web/src/shared/settings/store.ts
@@ -7,11 +7,13 @@ import { SETTINGS_COOKIE_NAME } from "./constants";
 export type Settings = {
   locale?: Locale;
   theme?: Theme;
+  snowfall?: boolean;
 };
 
 export const getDefaultSettings = (): Settings => ({
   locale: getUserLocale(),
   theme: "system",
+  snowfall: true,
 });
 
 export const createSettingsStore = () => createStore(getDefaultSettings());
---
+++ apps/web/src/widgets/snowfall.tsx
@@ -0,0 +1,37 @@
+import { createSignal, onMount, Show } from "solid-js";
+
+import { Fade } from "@quotepedia/solid";
+import { clientOnly } from "@solidjs/start";
+import { useSettings } from "~/shared/settings";
+
+// @ts-ignore
+const Root = clientOnly(() => import("solid-snowfall"));
+
+export const Snowfall = () => {
+  const settings = useSettings();
+
+  const [images, setImages] = createSignal<Array<HTMLImageElement>>();
+
+  onMount(() => {
+    const img = document.createElement("img");
+    img.src = "/img/snowflake.png";
+
+    setImages([img]);
+  });
+
+  return (
+    <Fade duration={300}>
+      <Show when={settings.store.snowfall}>
+        <Root
+          images={images()}
+          snowflakeCount={50}
+          radius={[10, 30]}
+          speed={[0, 3.0]}
+          wind={[-0.5, 2.0]}
+          rotationSpeed={[-0.5, 1.0]}
+          style={{ "z-index": 9999 }}
+        />
+      </Show>
+    </Fade>
+  );
+};
