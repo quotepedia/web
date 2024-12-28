3e1aaf63a9b3ed461684abb6e554cc180ced1138
zobweyt <zobweyt@gmail.com>
2024-12-28 20:15:06 +0300

    fix(web): properly use children

--- apps/web/src/routes/(app).tsx
+++ apps/web/src/routes/(app).tsx
@@ -1,10 +1,7 @@
 import type { RouteSectionProps } from "@solidjs/router";
-import { children } from "solid-js";
 import { Aside } from "~/widgets/aside";
 
 export default (props: RouteSectionProps) => {
-  const c = children(() => props.children);
-
   return (
     <main class="relative flex w-full max-lg:flex-col-reverse">
       <Aside />
