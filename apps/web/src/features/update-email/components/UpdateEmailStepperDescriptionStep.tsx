efb49a8533714cba8dea8cc1c914fdc3c6a6e948
zobweyt <zobweyt@gmail.com>
2025-01-02 20:18:22 +0300

    feat(web): update description steps

--- apps/web/src/features/update-email/components/UpdateEmailStepperDescriptionStep.tsx
+++ apps/web/src/features/update-email/components/UpdateEmailStepperDescriptionStep.tsx
@@ -1,45 +1,35 @@
-import { Button, Heading, Link, Lottie, Stack, Stepper, Text } from "@quotepedia/solid";
-import { A, createAsync } from "@solidjs/router";
-import { Show } from "solid-js";
+import { Button, Heading, Lottie, Stack, Stepper, Text } from "@quotepedia/solid";
+import { A } from "@solidjs/router";
 import FormStepper from "~/entities/form-stepper";
-import { getCurrentUser } from "~/entities/user";
 import { useTranslator } from "~/shared/i18n";
 
 export const UpdateEmailStepperDescriptionStep = () => {
   const t = useTranslator();
   const formStepper = FormStepper.useContext();
-  const currentUser = createAsync(() => getCurrentUser());
 
   return (
     <Stepper.Step onEnter={() => formStepper.setForm(undefined)}>
-      <Show when={currentUser()}>
-        {(user) => (
-          <Stack.Vertical class="gap-6 text-center">
-            <Lottie path="/tgs/envelope.json" class="size-24" />
+      <Stack.Vertical class="gap-12 text-center">
+        <Lottie path="/tgs/envelope.json" class="size-28" />
 
-            <Stack.Vertical>
-              <Heading>{t("components.changeEmail.steps.description.heading")}</Heading>
-              <Text>
-                {t("components.changeEmail.steps.description.current")}{" "}
-                <Link href={`mailto:${user().email}`}>{user().email}</Link>
-              </Text>
-            </Stack.Vertical>
+        <hgroup class="space-y-4 text-center">
+          <Heading>{t("components.changeEmail.steps.description.heading")}</Heading>
+          <Text>{t("components.changeEmail.steps.description.description")}</Text>
+        </hgroup>
 
-            <Text>{t("components.changeEmail.steps.description.description")}</Text>
+        <Stack.Vertical>
+          <Stepper.Forward as={Button} spacing="lg" class="w-full">
+            {t("continue")}
+          </Stepper.Forward>
+          <Button as={A} href="/settings/account" style="ghost" spacing="lg" class="w-full">
+            {t("cancel")}
+          </Button>
+        </Stack.Vertical>
 
-            <Text variant="muted">{t("components.changeEmail.steps.description.hint")}</Text>
-
-            <Stack.Vertical>
-              <Stepper.Forward as={Button} class="w-full">
-                {t("continue")}
-              </Stepper.Forward>
-              <Button as={A} href="/settings/account" style="ghost" class="w-full">
-                {t("back")}
-              </Button>
-            </Stack.Vertical>
-          </Stack.Vertical>
-        )}
-      </Show>
+        <Text variant="muted" size="sm">
+          {t("components.changeEmail.steps.description.hint")}
+        </Text>
+      </Stack.Vertical>
     </Stepper.Step>
   );
 };
