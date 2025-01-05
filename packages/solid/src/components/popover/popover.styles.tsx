ce682f8fd5539a34f7454dd9b159837e8148a822
zobweyt <zobweyt@gmail.com>
2025-01-05 17:25:53 +0300

    feat(ui): increase border radius in popover

--- packages/solid/src/components/popover/popover.styles.tsx
+++ packages/solid/src/components/popover/popover.styles.tsx
@@ -7,7 +7,7 @@ export const styles = tv({
       "origin-[var(--kb-popper-content-transform-origin)] transition",
       "data-[closed]:animate-out data-[closed]:fade-out-0 data-[closed]:zoom-out-0 data-[closed]:duration-300 data-[closed]:ease-in-out",
       "data-[expanded]:animate-in data-[expanded]:fade-in-0 data-[expanded]:zoom-in-0 data-[expanded]:duration-500 data-[expanded]:ease-spring",
-      "rounded-lg border border-bg-secondary bg-bg-default/75 backdrop-blur-lg shadow-md outline-none",
+      "rounded-xl border border-bg-secondary bg-bg-default/75 backdrop-blur-lg shadow-md outline-none",
     ],
   },
 });
