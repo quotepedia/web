ceee7df3a6e0d62a6a7f593e5bb96e41f25dda12
zobweyt <zobweyt@gmail.com>
2025-01-11 00:30:16 +0300

    perf(web): optimize current user actions

--- apps/web/src/lib/api/users/me/actions.ts
+++ apps/web/src/lib/api/users/me/actions.ts
@@ -1,15 +1,16 @@
-import { action } from "@solidjs/router";
+import { action, json } from "@solidjs/router";
 import { client } from "../../instance";
 import { serializeFormData } from "../../serializers";
 import type { components } from "../../types";
+import { getCurrentUser } from "./cache";
 
 export const removeCurrentUserAvatarAction = action(async () => {
   "use server";
 
   const { data, error } = await client.DELETE("/users/me/avatar");
 
-  return { data, error };
-});
+  return json({ data, error }, { revalidate: getCurrentUser.key });
+}, "remove-current-user-avatar-action");
 
 export const updateCurrentUserAvatarAction = action(async (file: File) => {
   "use server";
@@ -19,21 +20,24 @@ export const updateCurrentUserAvatarAction = action(async (file: File) => {
     bodySerializer: serializeFormData,
   });
 
-  return { data, error };
-});
+  return json({ data, error }, { revalidate: getCurrentUser.key });
+}, "update-current-user-avatar-action");
 
-export const updateCurrentUserEmailAction = action(async (body: components["schemas"]["CurrentUserEmailUpdateRequest"]) => {
-  "use server";
+export const updateCurrentUserEmailAction = action(
+  async (body: components["schemas"]["CurrentUserEmailUpdateRequest"]) => {
+    "use server";
 
-  const { data, error } = await client.PATCH("/users/me/email", { body: body });
+    const { data, error } = await client.PATCH("/users/me/email", { body: body });
 
-  return { data, error };
-});
+    return json({ data, error }, { revalidate: getCurrentUser.key });
+  },
+  "update-current-user-email-action",
+);
 
 export const updateCurrentUserPasswordAction = action(async (body: components["schemas"]["UserPasswordRequest"]) => {
   "use server";
 
   const { data, error } = await client.PATCH("/users/me/password", { body: body });
 
-  return { data, error };
-});
+  return json({ data, error }, { revalidate: getCurrentUser.key });
+}, "update-current-user-password-action");
