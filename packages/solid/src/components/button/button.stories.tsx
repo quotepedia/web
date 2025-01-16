87c5f9d655a87a8a353d69d1c61baf1cc6ba4609
zobweyt <zobweyt@gmail.com>
2025-01-16 22:22:58 +0300

    chore(ui): add colors button stories

--- packages/solid/src/components/button/button.stories.tsx
+++ packages/solid/src/components/button/button.stories.tsx
@@ -1,5 +1,6 @@
 import type { OverrideComponentProps } from "@kobalte/core";
 import type { Meta, StoryObj } from "@storybook/html";
+import { For } from "solid-js";
 import { Button, type RootProps } from "./index";
 
 type Story = StoryObj<OverrideComponentProps<"button", RootProps>>;
@@ -11,6 +12,27 @@ export const Default: Story = {
   },
 };
 
+export const Colors: Story = {
+  args: {
+    children: "Play",
+    leadingIcon: "f7:play-fill",
+  },
+  // @ts-ignore
+  render: (props) => (
+    <div class="flex flex-col gap-4">
+      <For each={["default", "bezeled", "ghost"] as const}>
+        {(style) => (
+          <div class="flex items-center space-x-4">
+            <For each={["primary", "success", "secondary", "danger"] as const}>
+              {(color) => <Button {...props} style={style} color={color} />}
+            </For>
+          </div>
+        )}
+      </For>
+    </div>
+  ),
+};
+
 export default {
   title: "Button",
   argTypes: {
@@ -18,9 +40,20 @@ export default {
       options: ["default", "bezeled", "ghost"],
       control: { type: "select" },
     },
+    spacing: {
+      options: ["sm", "md", "lg"],
+      control: { type: "select" },
+    },
+    shape: {
+      options: ["circle", "default"],
+      control: { type: "select" },
+    },
     color: {
       options: ["primary", "secondary", "success", "danger"],
       control: { type: "select" },
+    },
+    stretched: {
+      control: { type: "boolean" },
     },
     loading: {
       control: { type: "boolean" },
