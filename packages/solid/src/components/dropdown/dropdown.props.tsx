b343b96888b965ebe3c39cbd4c48ab9eca98767f
zobweyt <zobweyt@gmail.com>
2025-02-07 22:17:23 +0300

    feat: add sub dropdown support to dropdown

--- packages/solid/src/components/dropdown/dropdown.props.tsx
+++ packages/solid/src/components/dropdown/dropdown.props.tsx
@@ -1,10 +1,16 @@
 import type { JSX, ParentProps, ValidComponent } from "solid-js";
 
-import type { DropdownMenuContentProps, DropdownMenuItemProps } from "@kobalte/core/dropdown-menu";
+import type {
+  DropdownMenuContentProps,
+  DropdownMenuItemProps,
+  DropdownMenuCheckboxItemProps,
+} from "@kobalte/core/dropdown-menu";
 
 export type DropdownContentProps<T extends ValidComponent = "div"> = ParentProps<
   DropdownMenuContentProps<T> & JSX.StylableSVGAttributes
 >;
 
-export type DropdownItemProps<T extends ValidComponent = "div"> = DropdownMenuItemProps<T> &
+export type DropdownItemProps<T extends ValidComponent = "div"> = DropdownMenuItemProps<T> & JSX.StylableSVGAttributes;
+
+export type DropdownCheckboxItemProps<T extends ValidComponent = "div"> = DropdownMenuCheckboxItemProps<T> &
   JSX.StylableSVGAttributes;
--- packages/solid/src/components/dropdown/dropdown.tsx
+++ packages/solid/src/components/dropdown/dropdown.tsx
@@ -4,11 +4,13 @@ import * as DropdownPrimitive from "@kobalte/core/dropdown-menu";
 import type { PolymorphicProps } from "@kobalte/core/polymorphic";
 
 import { cn } from "@src/utils/css";
-import type { DropdownContentProps, DropdownItemProps } from "./dropdown.props";
+import type { DropdownCheckboxItemProps, DropdownContentProps, DropdownItemProps } from "./dropdown.props";
 import { styles } from "./dropdown.styles";
 
 // TODO: Re-map more dropdown primitives.
 const DropdownRoot = DropdownPrimitive.Root;
+const DropdownSub = DropdownPrimitive.Sub;
+const DropdownSubTrigger = DropdownPrimitive.SubTrigger;
 const DropdownTrigger = DropdownPrimitive.Trigger;
 const DropdownArrow = DropdownPrimitive.Arrow;
 const DropdownItemLabel = DropdownPrimitive.ItemLabel;
@@ -19,10 +21,21 @@ const DropdownContent = <T extends ValidComponent = "div">(props: PolymorphicPro
   const [, rest] = splitProps(props as DropdownContentProps, ["class"]);
   return (
     <DropdownPrimitive.Portal>
-      <DropdownPrimitive.Content class={cn(styles().content(), props.class)} {...rest} >
+      <DropdownPrimitive.Content class={cn(styles().content(), props.class)} {...rest}>
         <DropdownPrimitive.Arrow size={36} />
         {props.children}
       </DropdownPrimitive.Content>
+    </DropdownPrimitive.Portal>
+  );
+};
+
+const DropdownSubContent = <T extends ValidComponent = "div">(props: PolymorphicProps<T, DropdownContentProps<T>>) => {
+  const [, rest] = splitProps(props as DropdownContentProps, ["class"]);
+  return (
+    <DropdownPrimitive.Portal>
+      <DropdownPrimitive.SubContent class={cn(styles().content(), props.class)} {...rest}>
+        {props.children}
+      </DropdownPrimitive.SubContent>
     </DropdownPrimitive.Portal>
   );
 };
@@ -32,9 +45,20 @@ const DropdownItem = <T extends ValidComponent = "div">(props: PolymorphicProps<
   return <DropdownPrimitive.Item class={cn(styles().item(), props.class)} {...rest} />;
 };
 
+const DropdownCheckboxItem = <T extends ValidComponent = "div">(
+  props: PolymorphicProps<T, DropdownCheckboxItemProps<T>>,
+) => {
+  const [, rest] = splitProps(props as DropdownCheckboxItemProps, ["class"]);
+  return <DropdownPrimitive.CheckboxItem class={cn(styles().item(), props.class)} {...rest} />;
+};
+
 export const Dropdown = Object.assign(DropdownRoot, {
   Trigger: DropdownTrigger,
   Content: DropdownContent,
+  Sub: DropdownSub,
+  SubTrigger: DropdownSubTrigger,
+  SubContent: DropdownSubContent,
+  CheckboxItem: DropdownCheckboxItem,
   Item: DropdownItem,
   Arrow: DropdownArrow,
   ItemLabel: DropdownItemLabel,
